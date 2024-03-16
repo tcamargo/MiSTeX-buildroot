778b9d9c2898df6cfe4890ba3088010035513886
Tiago Pierezan Camargo <camargo@discworld.com.br>
2024-03-16 09:44:00 -0300

    Add external buildroot tree for all boards
    Instead of having each board with its own tree, it possible to provide custom parametes, like uboot and kernel, in config files.

---
+++ external.mk
@@ -0,0 +1,1 @@
+include $(sort $(wildcard $(BR2_EXTERNAL_MISTEX_PATH)/package/mistex/*/*.mk $(BR2_EXTERNAL_MISTEX_PATH)/package/mistex/*/*/*.mk $(BR2_EXTERNAL_MISTEX_PATH)/package/mistex/*/*/*/*.mk $(BR2_EXTERNAL_MISTEX_PATH)/package/mistex/*/*/*/*/*.mk))
